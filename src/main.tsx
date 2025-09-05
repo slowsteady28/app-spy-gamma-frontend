837526a908d5a8f6a12f88f1e02d150f62c5e1f1
Eliezer Nunez <eliezernunezjr@eoutlook.com>
2025-09-05 12:05:43 -0400

    debugging vercel

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
-import { ChartSyncProvider } from "./context/ChartSyncContext"; // FIXED
+import { ChartSyncProvider } from "./context/ChartSyncContext"; // FIX
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
